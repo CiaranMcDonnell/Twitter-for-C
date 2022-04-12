2561de30c6d304120d9084c114395009e88f6fc4
Ruairi <71946053+ruairifagan@users.noreply.github.com>
2022-04-12 15:33:51 +0100

    Beginning of menu

--- account.h
+++ account.h
@@ -2,6 +2,9 @@
 #define SE1TWITTERPROJ_ACCOUNT_H
 #endif //SE1TWITTERPROJ_ACCOUNT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 //protypes
 void makeAccounts(void);
 void deleteAccount(void);
--- main.c
+++ main.c
@@ -2,7 +2,45 @@
 #include "feed.h"
 #include "u_int.h"
 
+<<<<<<< Updated upstream
 int main(){
     
+=======
+void menuTable(void);
+void line(void);
+void row(char* text1, char* text2);
+
+int main(){
+    int endTwitter = 0;
+    printf("\nWelcome to...\nTwitter in C!\n");
+    // makeAccounts();
+    menuTable();
+    // while (endTwitter = 0){
+    //     printf("\nWould you like to: \n\n");
+    //     menu_table();
+    // }
+>>>>>>> Stashed changes
     return 0;
+}
+
+void menuTable(void){
+    char menuOptions[6][15] = {{"View News Feed"}, {"Follow a user"}, {"Unfollow a user"}, {"Delete Account"}, {"End Turn"}, {"End Twitter"}}, menuNums[6][2] = {{"1."}, {"2."}, {"3."}, {"4."}, {"5."}, {"6."}};
+    line();
+    row("No.", "Menu");
+    for (int i=0; i<2;i++){
+        line();
+        row(menuNums[i], menuOptions[i]);
+    }
+    line();
+}
+
+void line(void){
+    for (int i=0;i<28;i++){
+        printf("=");
+    }
+    printf("\n");
+}
+
+void row(char* text1, char* text2){
+    printf("||%4s ||%16s ||\n", text1, text2);
 }
