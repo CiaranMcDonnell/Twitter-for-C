519664f1008c64157391dba115839fae08cfc4ab
Ciarán <51961306+CiaranMcDonnell@users.noreply.github.com>
2022-05-03 04:30:48 +0100

    formatting

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ N/A
 ## License
 Ciarán McDonnell (ciaranmcdonnell@protonmail.com) <br />
 Ruairí Fagan (ruairi15fagan@gmail.com)
+
+
+
+## Notes
+The majority of work on this document was made using the Live Share feature on Visual Studio Code on Discord Calls.
+This improved our productivity and allows us to work on problems collaboratively however with the downside of less accurate commit history changes.
+As due to this all of the commits history would be credited to the Live Share host.
--- account.h
+++ account.h
@@ -1,20 +1,20 @@
 #ifndef SE1TWITTERPROJ_ACCOUNT_H
 #define SE1TWITTERPROJ_ACCOUNT_H
-#endif //SE1TWITTERPROJ_ACCOUNT_H
+#endif // SE1TWITTERPROJ_ACCOUNT_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct accountsNode {
+typedef struct accountsNode
+{
 	char username[15];
 	struct accountsNode *nextPtr;
 } accountNode;
 
 typedef accountNode *accountNodePtr; /* synonym for ListNode* */
 
-
-//protypes
+// protypes
 void makeAccounts(accountNodePtr *startPtr);
 void printAccounts(accountNodePtr startPtr);
 void deleteAccount(void);
