f2d40a9490492105d5198be528fe14389f093b3b
Ciarán <51961306+CiaranMcDonnell@users.noreply.github.com>
2022-05-03 16:37:33 +0100

    .

--- account.c
+++ account.c
@@ -1,38 +1,31 @@
 #include "account.h"
 
-void makeAccounts(accountNodePtr *startPtr)
-{
+void makeAccounts(accountNodePtr *startPtr){
     accountNodePtr endPtr = NULL;
     accountNodePtr newPtr;
-    char cont = 'y', uname[15];
-    while (cont == 'y' || cont == 'Y')
-    {
-        newPtr = (accountNode *)malloc(sizeof(accountNode));
-        if (newPtr != NULL)
-        {
+    char cont='y', uname[15];
+    while (cont=='y' || cont == 'Y'){
+        newPtr = (accountNode*)malloc(sizeof(accountNode));
+        if (newPtr != NULL) {
             accountName(*startPtr, uname);
             strcpy(newPtr->username, uname);
             newPtr->nextPtr = NULL;
             accountNode *previousPtr = endPtr;
-            if (*startPtr == NULL)
-            {
+            if (*startPtr==NULL){
                 *startPtr = newPtr;
             }
-            if (previousPtr != NULL)
-            {
+            if (previousPtr!=NULL){
                 previousPtr->nextPtr = newPtr;
             }
             endPtr = newPtr;
         }
-        else
-        {
+        else {
             printf("No memory available.\n");
         }
         printf("\nWould you like to make another account? y/n: ");
         fflush(stdin);
         scanf("%c", &cont);
-        while (cont != 'n' && cont != 'N' && cont != 'y' && cont != 'Y')
-        {
+        while (cont != 'n' && cont != 'N' && cont != 'y' && cont != 'Y'){
             fflush(stdin);
             printf("Invalid input\nPlease try again\n\nWould you like to make another account? y/n: ");
             scanf("%c", &cont);
@@ -40,59 +33,49 @@ void makeAccounts(accountNodePtr *startPtr)
     }
 }
 
-void accountName(accountNodePtr startPtr, char *uname)
-{
+void accountName(accountNodePtr startPtr, char *uname){
     accountNodePtr testPtr;
     char attempt[16];
     int len, dq = 1;
-    while (dq != 0)
-    {
+    while (dq!=0){
         testPtr = startPtr;
-        dq = 0;
+        dq=0;
         printf("\n(Usernames are limited to 15 characters and only letters and numbers are permitted)\n");
         printf("Enter: ");
         fflush(stdin);
-        scanf("%s", attempt);
+        scanf("%s", &attempt);
         len = strlen(attempt);
-        if (len > 15)
-        {
+        if(len>15){
             printf("Your input included more than 15 characters\nPlease try again\n\n");
             dq++;
         }
-        if (dq != 1)
-        {
-            for (int i = 0; i < len; i++)
-            {
-                if (((attempt[i] > 'Z' || attempt[i] < 'A') && (attempt[i] > 'z' || attempt[i] < 'a') && (attempt[i] > '9' || attempt[i] < '0')) && attempt[i] != '_')
-                {
+        if (dq!= 1){
+            for (int i=0; i<len;i++){
+                if (((attempt[i] > 'Z' || attempt[i]<'A') &&  (attempt[i]>'z' || attempt[i]<'a') &&  (attempt[i]>'9' || attempt[i]<'0')) && attempt[i]!='_'){
                     dq++;
                     printf("Your input contains illegal characters\nPlease try again\n\n");
                     break;
                 }
             }
         }
-        while (testPtr != NULL && dq != 1)
-        {
-            if (strcmp(testPtr->username, attempt) == 0)
-            {
+        while (testPtr!=NULL && dq!=1) {
+            if(strcmp(testPtr->username, attempt) == 0){
                 dq++;
                 printf("Sorry, this username already exists\nPlease try again\n\n");
             }
             testPtr = testPtr->nextPtr;
-        }
+        }    
     }
-    strcpy(uname, attempt);
+    strcpy(uname,attempt);
 }
 
-void printAccounts(accountNodePtr startPtr)
-{
-    while (startPtr != NULL)
-    {
-        printf("Account name: %s\n", startPtr->username);
-        startPtr = startPtr->nextPtr;
-    }
+void printAccounts(accountNodePtr startPtr){
+    while (startPtr!=NULL) {
+		printf("Account name: %s\n", startPtr->username);
+		startPtr = startPtr->nextPtr;
+	}
 }
 
-void deleteAccount(void)
-{
+void deleteAccount(void){
+
 }
--- main.c
+++ main.c
@@ -4,51 +4,42 @@
 
 void menuTable(void);
 void line(void);
-void row(char *text1, char *text2);
+void row(char* text1, char* text2);
 
-int main()
-{
+int main(){
     int endTwitter = 0;
     accountNode *startPtr = NULL;
     printf("\nWelcome to...\nTwitter in C!\n");
     makeAccounts(&startPtr);
     printAccounts(startPtr);
-    // menuTable();
-    //  while (endTwitter = 0){
-    //      printf("\nWould you like to: \n\n");
-    //      menu_table();
-    //  }
+    //menuTable();
+    // while (endTwitter = 0){
+    //     printf("\nWould you like to: \n\n");
+    //     menu_table();
+    // }
     return 0;
 }
 
-/**
- * It prints a table with the menu options.
- */
-void menuTable(void)
-{
+void menuTable(void){
     char menuOptions[6][16] = {{"View News Feed\0"}, {"Follow a user\0"}, {"Unfollow a user\0"}, {"Delete Account\0"}, {"End Turn\0"}, {"End Twitter\0"}};
     char menuNums[6][3] = {{"1.\0"}, {"2.\0"}, {"3.\0"}, {"4.\0"}, {"5.\0"}, {"6.\0"}};
     line();
     row("No.", "Menu");
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i=0; i<5;i++){
         line();
-        int k = 1;
+        int k=1;
         row(menuNums[i], menuOptions[i]);
     }
     line();
 }
 
-void line(void)
-{
-    for (int i = 0; i < 28; i++)
-    {
+void line(void){
+    for (int i=0;i<28;i++){
         printf("=");
     }
     printf("\n");
 }
 
-void row(char *text1, char *text2)
-{
+void row(char* text1, char* text2){
     printf("||%4s ||%16s ||\n", text1, text2);
 }
